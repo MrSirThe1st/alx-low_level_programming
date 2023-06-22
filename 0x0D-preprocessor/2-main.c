115e028e7f1e3e1b7572388f699b7f201b6451a6
MrSirThe1st <marcilungambuyu@gmail.com>
2023-06-22 15:01:07 -0700

    dwe

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - role ios toprints the name of the file
+ *
+ * Return: Alwayfs 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#define SUM_H
+#ifndef SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
